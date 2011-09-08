b51f609775c52a8f9ac54ee683f56156aa31bc4a
Weisbeck <jweisbeck@boston.com>
2011-09-08 11:33:57 -0400

    Put a real puzzle into script.js

--- js/script.js
+++ js/script.js
@@ -5,60 +5,148 @@
 		// two entries: an across entry and a down entry
 		var puzzleData = [
 			 	{
-					clue: "Checked by a robot",
-					answer: "scanned",
-					position: 5,
-					orientation: "down",
-					startx: 5,
+					clue: "First letter of greek alphabet",
+					answer: "alpha",
+					position: 1,
+					orientation: "across",
+					startx: 1,
 					starty: 1
 				},
 			 	{
-					clue: "With Your Ears",
-					answer: "sound",
+					clue: "Not a one ___ motor, but a three ___ motor",
+					answer: "phase",
 					position: 3,
 					orientation: "across",
-					startx: 2,
-					starty: 4
-				},
-				{
-					clue: "Favorite Pet",
-					answer: "hamster",
-					position: 1,
-					orientation: "down",
-					startx: 2,
+					startx: 7,
 					starty: 1
 				},
 				{
-					clue: "Least favorite pet",
-					answer: "cat",
-					position: 2,
+					clue: "Created from a separation of charge",
+					answer: "capacitance",
+					position: 5,
 					orientation: "across",
 					startx: 1,
-					starty: 2
+					starty: 3
 				},
 				{
-					clue: "Gain knowledge",
-					answer: "read",
-					position: 4,
+					clue: "The speeds of engines without and accelaration",
+					answer: "idlespeeds",
+					position: 8,
+					orientation: "across",
+					startx: 1,
+					starty: 5
+				},
+				{
+					clue: "Complex resistances",
+					answer: "impedances",
+					position: 10,
 					orientation: "across",	
 					startx: 2,
 					starty: 7
 				},
 				{
-					clue: "You can only wear one",
-					answer: "Hats",
-					position: 1,
+					clue: "This device is used to step-up, step-down, and/or isolate",
+					answer: "transformer",
+					position: 13,
 					orientation: "across",
-					startx: 2,
+					startx: 1,
+					starty: 9
+				},
+				{
+					clue: "Type of ray emitted frm the sun",
+					answer: "gamma",
+					position: 16,
+					orientation: "across",
+					startx: 1,
+					starty: 11
+				},
+				{
+					clue: "C programming language operator",
+					answer: "cysan",
+					position: 17,
+					orientation: "across",
+					startx: 7,
+					starty: 11
+				},
+				{
+					clue: "Defines the alpha-numeric characters that are typically associated with text used in programming",
+					answer: "ascii",
+					position: 1,
+					orientation: "down",
+					startx: 1,
 					starty: 1
 				},
 				{
-					clue: "Supporting evidence",
-					answer: "corroborate",
-					position: 6,
-					orientation: "across",
+					clue: "Generally, if you go over 1kV per cm this happens",
+					answer: "arc",
+					position: 2,
+					orientation: "down",
 					startx: 5,
-					starty: 2
+					starty: 1
+				},
+				{
+					clue: "Control system strategy that tries to replicate the human through process (abbr.)",
+					answer: "ann",
+					position: 4,
+					orientation: "down",
+					startx: 9,
+					starty: 1
+				},
+				{
+					clue: "Greek variable that usually describes rotor positon",
+					answer: "theta",
+					position: 6,
+					orientation: "down",
+					startx: 7,
+					starty: 3
+				},
+				{
+					clue: "Electromagnetic (abbr.)",
+					answer: "em",
+					position: 7,
+					orientation: "down",
+					startx: 11,
+					starty: 3
+				},
+				{
+					clue: "No. 13 across does this to a voltage",
+					answer: "steps",
+					position: 9,
+					orientation: "down",
+					startx: 5,
+					starty: 5
+				},
+				{
+					clue: "Emits a lout wailing sound",
+					answer: "siren",
+					position: 11,
+					orientation: "down",
+					startx: 11,
+					starty: 7
+				},
+				{
+					clue: "Information technology (abbr.)",
+					answer: "it",
+					position: 12,
+					orientation: "down",
+					startx: 1,
+					starty: 8
+				},
+				{
+					clue: "Asynchronous transfer mode (abbr.)",
+					answer: "atm",
+					position: 14,
+					orientation: "down",
+					startx: 3,
+					starty: 9
+				},
+				{
+					clue: "Offset current control (abbr.)",
+					answer: "occ",
+					position: 15,
+					orientation: "down",
+					startx: 7,
+					starty: 9
 				}
 			] 
 	
